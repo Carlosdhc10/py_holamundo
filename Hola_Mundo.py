d0dd5398048e9e64ae8792500a7e8ae330f2d5ee
Carlosdhc10 <cdhernandezc@uce.edu.ec>
2024-11-17 11:40:47 -0500

    Refactor file

--- Hola_Mundo.py
+++ Hola_Mundo.py
@@ -1,3 +1,12 @@
-#hola mundo en python
+from flask import Flask
 
-print('hola mundo')
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hola Mundo desde Heroku!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
